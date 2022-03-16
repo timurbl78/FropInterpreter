e1ac4543ca7d9fd7c5d54bde888e55834bf566bc
sokratmillman <superstepashka1981@gmail.com>
2022-03-16 20:07:30 +0300

    Add comments support

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -3,20 +3,35 @@ package Main;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
+import java.io.File; 
+import java.io.FileNotFoundException;  
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.io.FileWriter;
 
-import Main.Element.Element;
-import Main.Interpreter.Interpreter;
+import Main.Interpreter.*;
 import Main.Types.*;
-import Main.parser.Parser;
+import Main.Element.*;
+import Main.parser.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         if (args.length == 0) {
             System.out.println("You should specify the path to the file f");
             return;
         }
+        
+        String pathToFile = args[0];
+        File newFile = tempWithoutComments(pathToFile);
 
-        ElementsList ast = Parser.buildAST(args[0]);
+        if (newFile.length()==0) {
+            System.out.println("No such file, check the path");
+            return;
+        }
+
+        ElementsList ast = Parser.buildAST(newFile.getAbsolutePath());
 
         PrintWriter writer = new PrintWriter("AST.json", StandardCharsets.UTF_8);
         writer.print(ast);
@@ -24,7 +39,7 @@ public class Main {
 
         Interpreter interpreter = new Interpreter();
         System.out.println("AST tree was built. Please, check file 'FropInterpreter/AST.json'");
-
+        
         int i = 0;
         assert ast != null;
         for (Element elem : ast) {
@@ -41,4 +56,36 @@ public class Main {
             }
         }
     }
+    public static File tempWithoutComments(String pathToFile) throws IOException{
+        Pattern pattern = Pattern.compile("#.*$");
+         try {
+          File myFile = new File(pathToFile);
+          Scanner myReader = new Scanner(myFile);
+          StringBuffer bf = new StringBuffer();
+          String data = myReader.nextLine();
+          Matcher match = pattern.matcher(data);
+          data = match.replaceAll("");
+          bf.append(data);
+          while (myReader.hasNextLine()) {
+            data = myReader.nextLine();
+            match = pattern.matcher(data);
+            data = match.replaceAll("");
+            bf.append(System.lineSeparator()+data);
+          }
+          myReader.close();
+          String fileContents = bf.toString();
+          String tempFileName = myFile.getParent()+"\\temp"+myFile.getName();
+          File tempFile = File.createTempFile(tempFileName, "");
+          tempFile.getName();
+          FileWriter wr = new FileWriter(tempFile);
+          wr.write(fileContents);
+          wr.close();
+          tempFile.deleteOnExit();
+          return tempFile;
+        } catch (FileNotFoundException e) {
+          File notFoundCase = File.createTempFile(pathToFile, "");
+          notFoundCase.deleteOnExit();
+          return notFoundCase;
+        }
+      }
 }
